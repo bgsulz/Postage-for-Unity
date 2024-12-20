bcb1ada34ea31c9f417b8b7d90b25a1fa4640f98
bgsulz <bgs.mail1234@gmail.com>
2024-12-20 16:24:21 -0500

    Send methods on AddressSO

--- Runtime/AddressSO.cs
+++ Runtime/AddressSO.cs
@@ -11,5 +11,15 @@ namespace Extra.Postman
         public static implicit operator Address(AddressSO so) => Address.Get(so.Key);
 
         public void RefreshKey() => Key = Guid.NewGuid().ToString();
+
+        public void Send<T>() => Message.Send<T>(this);
+
+        // For UnityEvents:
+        public void Send() => Message.Send(this);
+        public void Send(int p) => Message.Send(this, p);
+        public void Send(float p) => Message.Send(this, p);
+        public void Send(string p) => Message.Send(this, p);
+        public void Send(bool p) => Message.Send(this, p);
+        public void Send(Object p) => Message.Send(this, p);
     }
 }
